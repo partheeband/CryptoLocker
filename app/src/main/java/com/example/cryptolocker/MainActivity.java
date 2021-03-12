ba1f69547f5171e1f66ec2fe332aea51d3961de0
Ramprasad <prasad814359@gmail.com>
2021-03-12 14:05:08 +0530

    welcome ram

--- app/src/main/java/com/example/cryptolocker/MainActivity.java
+++ app/src/main/java/com/example/cryptolocker/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /*no commits**/
+        System.out.println("Welcome ram")
     }
 }
